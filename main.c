2921352160f6ea0b3e093c32fd2f6aa48a0e3736
Maria Berestovaya <57846761+antaresofficial@users.noreply.github.com>
2023-03-06 00:57:56 +0500

    feat: upper char to !lower.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex.h>
+#include <ctype.h>
 
 int main(int argc, char* argv[]) {
   char *action = argv[1];
@@ -9,6 +11,9 @@ int main(int argc, char* argv[]) {
   if (!strcmp(action, "pack") && path) {
     FILE *input_file = fopen(path, "r");
     FILE *output_file = fopen("./workspace/output.vlc", "w+");
+
+    regex_t regex;
+    int reti;
 
     char str[128];
     char *content, *new_content;
@@ -20,14 +25,27 @@ int main(int argc, char* argv[]) {
 
     content = fgets(str, 126, input_file);
     int i = 0;
+    int k = 0;
+    char upper_formated[128];
 
-    // while(content[i] != '\0') {
-    //   if (content[i] == 'o') {
-    //     printf("%c \n", content[i]);
-    //   }
-    //   i++;
-    // }
-    // fprintf(output_file, "%s", str);
+    while(content[i] != '\0') {
+      char symb[1];
+      char upper[2] = "!";
+      *symb = content[i];
+
+      reti = regcomp(&regex, "[A-Z]", 0);
+      reti = regexec(&regex, symb, 0, NULL, 0);
+      if (!reti) {
+        upper_formated[k] = '!';
+        upper_formated[k + 1] = tolower(*symb);
+        k += 2;
+      } else {
+        upper_formated[k] = content[i];
+        k++;
+      }
+      i++;
+    }
+    printf("%s", upper_formated);
 
     fclose(input_file);
   } else {
